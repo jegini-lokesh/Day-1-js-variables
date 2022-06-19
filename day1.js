60a091a10e0e2487e6aa60e156cba5d940d73688
jegini <jeginilokesh012022@gmail.com>
2022-06-19 23:53:20 +0530

    day-1 js varibale var,let,const function scope is pending

---
+++ day1.js
@@ -0,0 +1,25 @@
+//var key word
+//point-1
+var a; //declare a variable with out value
+a = 10; //asigin a variable with a value
+//point-2-re declare a variable
+var b = 20;
+var b = 30; // re declare variable is taken.
+alert(b)
+    //------------------------------------------------------------------------------------
+    //let key word
+    //point -1
+let c; // declare a variable with out value 
+c = 60; //asigin a variable with a value when we required is posible // for asigin value to varibale/ let c=60;/ this is wrong it shows variable already declared.
+alert(c);
+//point-2-re declare a variable
+let d = 20; //declare a variable with value
+//let d = "30"; //re de declare a variable thrown error
+console.log(d);
+//-------------------------------------------------------------------
+//const key word
+const e; // declare a variable with out value
+e = 600; //asigin a variable with a value when we required
+alert(e)
+const f = 200; //declare a variable with value
+const f = 500; //re de declare a variable thrown errorcpcpcc
